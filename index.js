d5bd50327b31dbd2fe40c4e63753fafdf3adf974
sadik182 <sadikurrahman728@gmail.com>
2022-10-23 23:33:10 +0600

    Database Connected

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-const express = require('express');
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
-const cors = require('cors');
-const port = process.env.PORT || 5000
+const cors = require("cors");
+const port = process.env.PORT || 5000;
 
-// Midle ware 
-
+// Midle ware
+// Genius_Car
+//8dbb1ehjElACjEOm
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('From Car Service Server');
-})
+const uri =
+  "mongodb+srv://Genius_Car:8dbb1ehjElACjEOm@cluster0.crcll.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("Genius_Car_DB");
+    const dataCollection = database.collection("Services");
+    console.log("Databse is Connected");
+  } finally {
+    // client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("From Car Service Server");
+});
 
 app.listen(port, () => {
-    console.log('Listing From Port ', port);
-})
+  console.log("Listing From Port ", port);
+});
