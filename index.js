c31a2b63af84a39d3132a262d4aa8e8464a0ce5f
sadik182 <sadikurrahman728@gmail.com>
2022-10-27 23:55:05 +0600

    dotenv implemented

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const cors = require("cors");
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 // Midle ware
-// Genius_Car
-//8dbb1ehjElACjEOm
+
 app.use(cors());
 app.use(express.json());
 
 const uri =
-  "mongodb+srv://Genius_Car:8dbb1ehjElACjEOm@cluster0.crcll.mongodb.net/?retryWrites=true&w=majority";
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.crcll.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -28,7 +28,8 @@ async function run() {
     // Get All Services
     app.get('/Services', async(req, res) => {
       const query = {}
-      const result = await dataCollection.find(query)
+      const cursor =  dataCollection.find(query)
+      const result = await cursor.toArray();
       res.send(result);
     });
 
